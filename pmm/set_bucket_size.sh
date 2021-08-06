c0ec547229af820b1685f6c2c0c975afbe68cadd
David Eros <david.eros@nuance.com>
2021-08-06 16:23:14 +0200

    add pmm stuff

---
+++ pmm/set_bucket_size.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+memory=$(free --giga | grep Mem | awk '{print $2}')
+cur_buckets=$(cat /proc/sys/net/netfilter/nf_conntrack_buckets)
+cur_count=$(cat /proc/sys/net/netfilter/nf_conntrack_count)
+max_count=$(cat /proc/sys/net/netfilter/nf_conntrack_max)
+buckets=16384
+div=2
+
+echo ${memory}"[Gig] memory"
+echo ${buckets} "bucket"
+echo ${cur_count} "current count"
+echo ${max_count} "max count"
+
+if (( ${cur_count} > ${max_count} )); then
+  new_size=$( expr 2000 + ${cur_count})
+  sysctl -w net.netfilter.nf_conntrack_max=${new_size}
+  echo "net.netfilter.nf_conntrack_max=$new_size" >> /etc/sysctl.conf
+fi
